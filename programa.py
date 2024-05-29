83f84353ae2dd7fddea82d97984d51cfae9e288b
Kyoumas <144501527+kkyouma@users.noreply.github>
2024-05-29 16:50:11 -0400

    update filter method

---
+++ programa.py
@@ -0,0 +1,8 @@
+def calcular_area(base, altura):
+    return (base * altura) / 2
+
+
+def pedir_lados():
+    base = float(input("Ingresar la longitud: "))
+    altura = float(input("Ingresar la altura: "))
+    return base, altura
